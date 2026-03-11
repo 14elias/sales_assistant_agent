3a64b5b2befc0a11bfbdfa62949b4a216e8d51de
14elias <ellamebrahtom1995@gmail.com>
2026-03-11 22:37:10 +0300

    a function for cleaning audio files that are created before 1 hour or more than 1 hour

---
+++ cleaner.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+
+def cleanup_audio(folder='audio_responses', max_age=3600):
+    now = time.time()
+    for file in os.listdir(folder):
+        path = os.path.join(folder, file)
+        if os.path.isfile(path) and now - os.path.getmtime(path) > max_age:
+            os.remove(path)
+
+if __name__ == '__main__':
+    cleanup_audio()

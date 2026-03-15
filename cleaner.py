b106b7e3f19bd62df676c9f5061a0d5bc403cd36
14elias <ellamebrahtom1995@gmail.com>
2026-03-15 22:33:20 +0300

    helper function for removing log files and audio saved in the local storage

--- cleaner.py
+++ cleaner.py
@@ -9,5 +9,12 @@ def cleanup_audio(folder='audio_responses', max_age=3600):
         if os.path.isfile(path) and now - os.path.getmtime(path) > max_age:
             os.remove(path)
 
+def cleanup_logs(folder='logs', max_age=3600):
+    now = time.time()
+    for file in os.listdir(folder):
+        path = os.path.join(folder, file)
+        if os.path.isfile(path) and now - os.path.getmtime(path) > max_age:
+            os.remove(path)
+
 if __name__ == '__main__':
-    cleanup_audio()
+    cleanup_logs()

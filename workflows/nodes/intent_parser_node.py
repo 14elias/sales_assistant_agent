7b49270fa307c9547380f650a18f94a7857607db
14elias <ellamebrahtom1995@gmail.com>
2026-03-15 22:30:59 +0300

    making the agent statefull by storing the history of the conversation

--- workflows/nodes/intent_parser_node.py
+++ workflows/nodes/intent_parser_node.py
@@ -1,33 +1,44 @@
-from workflows.agent_state import AgentState
-from ...services import llm_service
-from ..prompts import ACTIONPLANNERSYSTEMPROMPT
-from ...schemas.action_schema import ActionSchema
+from langchain_core.messages import HumanMessage, SystemMessage
 
+from ..agent_state import AgentState
+from services import llm_service
+from ..prompts import ACTIONPLANNERSYSTEMPROMPT
+from schemas.action_schema import ActionSchema
+
+
+from utils import GLOBAL_LOGGER
 
 def intent_parser_node(state: AgentState):
+    logger = GLOBAL_LOGGER.bind(module='intent_parser_node')
+
+    history = state.get("messages", [])
+
+    user_text = state.get("user_input_text")
+
+    if not user_text:
+        logger.info('User input text is empty')
+        raise ValueError("User input text is empty")
+    
+    new_user_msg = HumanMessage(content=user_text)
 
     llm = llm_service.llm.with_structured_output(ActionSchema)
 
-    if not state["user_input_text"]:
-        raise ValueError("User input text is empty")
-
-    messages = [
-        {
-            "role": "system",
-            "content": ACTIONPLANNERSYSTEMPROMPT
-        },
-        {
-            "role": "user",
-            "content": state["user_input_text"]
-        }
+    llm_messages = [
+        SystemMessage(content=ACTIONPLANNERSYSTEMPROMPT),
+        *history,
+        new_user_msg
     ]
 
-    result: ActionSchema = llm.invoke(messages)
+    result: ActionSchema = llm.invoke(llm_messages)
 
     if not result.action:
+        logger.error('LLM failed to determine action')
         raise ValueError("LLM failed to determine action")
 
+    logger.info('the user input is parsed successfully', extra={"actions":result.action})
+
     return {
+        "messages": [new_user_msg],
         "action": result.action,
         "recipient_email": result.recipient_email,
         "datetime": result.datetime,
@@ -36,3 +47,9 @@ def intent_parser_node(state: AgentState):
         "subject": result.subject,
         "body": result.body
     }
+
+
+if __name__ == '__main__':
+    state = AgentState()
+    state['user_input_text'] = 'schedule an appointment for tommorow 4 oclock and inform ella@gmail.com about the appointement '
+    intent_parser_node(state)
